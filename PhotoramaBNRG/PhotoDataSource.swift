13bcc813977b4665dfbd9646423f8b37dd7523e8
AnatoliyChernyuk <toliatche@gmail.com>
2018-02-03 20:01:11 +0200

    1. Cell updated

---
+++ PhotoramaBNRG/PhotoDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoDataSource.swift
+//  PhotoramaBNRG
+//
+//  Created by Anatoliy Chernyuk on 2/3/18.
+//  Copyright © 2018 Anatoliy Chernyuk. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    var photos = [Photo]()
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let identifier = "PhotoCollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! PhotoCollectionViewCell
+        return cell
+    }
+    
+}
